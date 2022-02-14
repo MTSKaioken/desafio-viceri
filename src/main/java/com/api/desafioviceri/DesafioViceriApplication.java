1a26e31b0632e0a111af19ea230bfcddbf88167d
Kaio Ruan <Kaio.martins2@aluno.unip.br>
2022-02-14 15:05:37 -0300

    Inicio Projeto+ Dependencias

---
+++ src/main/java/com/api/desafioviceri/DesafioViceriApplication.java
@@ -0,0 +1,13 @@
+package com.api.desafioviceri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioViceriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioViceriApplication.class, args);
+	}
+
+}
